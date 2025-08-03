62a69910efda102b6882934fc9af3e5548522eb3
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2025-08-03 15:38:14 -0400

    refactor: changed db name

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./moving_app.db"
+DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
