53f3d2fdd066af0e1a981334ed9275d295a90d01
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2025-08-03 15:50:09 -0400

    fix: Not importing models

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-import app.models
+from app.models import order, route, truck
 from app.routers import upload
+import logging
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(upload.router, prefix="/api")
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+logger = logging.getLogger(__name__)
