c95c5866734385ae18856f1188f594bc6f918cc1
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2025-08-06 13:57:58 -0400

    feat: Set up MUI Theme Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#9c27b0',
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
-    </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
