1628949d666b21b533cbd4aa1ffdadeffa2becd0
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2025-08-07 14:36:15 -0400

    bug: Fixed blank page after creating route & truck

--- backend/schemas/truck.py
+++ backend/schemas/truck.py
@@ -11,6 +11,10 @@ class TruckUpdate(BaseModel):
     comments: Optional[str] = None
 
 class TruckRead(BaseModel):
+    id: int
+    model: Optional[str] = None
+    comments: Optional[str] = None
+
     class ConfigDict:
         from_attributes = True
 
