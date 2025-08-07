ac398426a90f04b3cff2deac93b9c31299ca5d1f
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2025-08-07 00:04:44 -0400

    feat: Added website title to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,12 +3,16 @@ import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Button from "@mui/material/Button"
 import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
 
 export default function Header() {
   return (
     <div>
       <AppBar position="static" color="primary" elevation={1}>
         <Toolbar>
+            <Typography variant="h6" sx={{ flexShrink: 0, mr: 4, fontStyle: "italic" }}>
+            SS Website
+            </Typography>
           <Box sx={{ flexGrow: 1, display: "flex", gap: 2 }}>
             <Button
               component={Link}
